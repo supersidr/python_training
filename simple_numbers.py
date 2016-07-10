14fbe5dce57bf91bc93d2f4208ff4bb72d3e26eb
Игорь <igor.s.sidorov@gmail.com>
2016-07-11 00:19:35 +0300

    simple num

--- simple_numbers.py
+++ simple_numbers.py
@@ -3,6 +3,17 @@ import math
 
 i = 0
 while i < 100:
-    if (i / 2 - math.floor(i / 2)) != 0 and (i / 3 - math.floor(i / 3)) != 0 and (i / 5 - math.floor(i / 5)) != 0:
+    if (i / 2 - math.floor(i / 2)) != 0 and (i / 3 - math.floor(i / 3)) != 0 and (i / 5 - math.floor(i / 5)) != 0 and (i / 7 - math.floor(i / 7)) != 0:
         print (i)
     i = i + 1
+
+n = 100
+lst=[]
+for i in range(2, n+1):
+    # пробегаем по списку (lst) простых чисел
+    for j in lst:
+        if i % j == 0:
+            break
+    else:
+        lst.append(i)
+print (lst)
